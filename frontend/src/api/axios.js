f6455331ec56efa807f6bc3e89bef929e8e514b8
toufikunsure-4529 <devtoufik@gmail.com>
2025-11-09 01:42:57 +0530

    `Update axios baseURL to include '/api'`

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
   withCredentials: true
 });
 export default api;
