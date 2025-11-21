b12d3060d2cd8d9ed24924c83e4a0aac109069c6
toufikunsure-4529 <devtoufik@gmail.com>
2025-11-22 04:17:01 +0530

    Fix bug

--- frontend/src/pages/auth/AdminLogin.jsx
+++ frontend/src/pages/auth/AdminLogin.jsx
@@ -294,6 +294,7 @@ export default function AdminLogin() {
                                 Forgot password?
                             </Link>
                         </div>
+                        
 
 
                     </div>
